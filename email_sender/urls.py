b08e5175cc2d9c88d0e08fbe436f6a524b55c8ab
IdrissaMaiga <145261070+IdrissaMaiga@users.noreply.github.com>
2025-08-11 20:07:07 +0200

    dsfsf
    sdfsf

--- email_sender/urls.py
+++ email_sender/urls.py
@@ -3,6 +3,8 @@ URL configuration for email_sender project.
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from email_monitor.views import webhook_endpoint_1, webhook_endpoint_2
 
 urlpatterns = [
@@ -13,3 +15,7 @@ urlpatterns = [
     path('webhook1/', webhook_endpoint_1, name='webhook_endpoint_1'),
     path('webhook2/', webhook_endpoint_2, name='webhook_endpoint_2'),
 ]
+
+# Serve static files in all environments (including production)
+if settings.STATIC_URL:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
